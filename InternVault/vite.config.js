d890dd9d4322d94716e96e608272dc08b5cdad58
praveen13-creator <22221a0482@gmail.com>
2025-12-18 14:17:48 +0530

    re ediit

--- InternVault/vite.config.js
+++ InternVault/vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   css: {
-    devSourcemap: true,
-    transformer: "postcss"
+    devSourcemap: true
   },
   build: {
-    sourcemap: true,
+    sourcemap: true
   }
-})
+});
